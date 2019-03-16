c7478adf914f81b87d848dd3145e64283f40653c
Kshitij Sinha <kshitij.sinha94@gmail.com>
2019-03-16 12:58:14 -0400

    Modieified LV behaviour
    LV now changes color on pressing s. This should be replaced by hand controller movement detection.

--- Assets/Scripts/LVController.cs
+++ Assets/Scripts/LVController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,29 +9,52 @@ public class LVController : MonoBehaviour
     private Vector3 LVposition, newLVPosition;
     private GameObject LV;
     private float increaseZPosition;
+    private int probLV;
     public float smoothTime = 0.3F;
     private float velocity = 1.0f;
-    public float eccentricity= 1.0f; // length from 0 to endpoint.
+    public float eccentricity = 1.0f; // length from 0 to endpoint.
+
+    private Renderer rend;
     void Awake()
     {
         LV = GameObject.FindGameObjectWithTag("LV");
         LVposition = LV.transform.position;
 
- }
+    }
 
     void Start()
     {
-        
+        rend = LV.GetComponent<Renderer> ();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown("s"))
+        {
+            StartCoroutine("Brake");
+        }
         //Here will be the check to see if 200 objects have ben populated. If yes,
         // Then the eccentricity will change aand so on.
-    
-        newLVPosition = new Vector3 (LVposition.x, LVposition.y, LVposition.z + Mathf.PingPong (velocity * Time.time, eccentricity));
+
+        /*
+            Generate a random number between 1 and 5 giving.
+         */
+        probLV = Random.Range(0, 3);
+
+
+        float time = Time.time;
+        newLVPosition = new Vector3(LVposition.x, LVposition.y, LVposition.z + Mathf.PingPong(velocity * Time.time, eccentricity));
+        Debug.Log(rend);
         LV.transform.position = newLVPosition;
 
+    }
+
+
+    IEnumerator Brake()
+    {
+      LV.GetComponent<Renderer>().material.color = Color.red;
+      yield return new WaitForSeconds(.1f);
     }
 }
