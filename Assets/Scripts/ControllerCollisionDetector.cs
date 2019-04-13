9290ffbfd5c54ef32c9b841b6c6e22c96b6b9a5b
Kshitij Sinha <kshitij.sinha94@gmail.com>
2019-04-12 23:27:31 -0400

    Added HTC Vive Input Utility

--- Assets/Scripts/ControllerCollisionDetector.cs
+++ Assets/Scripts/ControllerCollisionDetector.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class ControllerCollisionDetector : MonoBehaviour
 {
@@ -48,7 +49,7 @@ public class ControllerCollisionDetector : MonoBehaviour
 
         if (other.gameObject.CompareTag("Target"))
         {
-             Debug.Log("Collision");
+            Debug.Log("Collision");
 
             // Set the Target as inactive
             other.gameObject.SetActive(false);
@@ -70,11 +71,16 @@ public class ControllerCollisionDetector : MonoBehaviour
         // Enter this is the Object collided with is the start area
         if (other.gameObject.CompareTag("StartArea"))
         {
-            Debug.Log("Collision Start Area");
-            other.gameObject.SetActive(false); // Deactivate the start area
-            targetSpawnnerScript.inStartArea = true; // Set the flag to indicate the controllre is now in the start area
-            initTime = Time.time; // set the start time
-            //Debug.Log("Init Time: " + initTime);
+            //if ()
+            //{
+                Debug.Log("Collision Start Area");
+                other.gameObject.SetActive(false); // Deactivate the start area
+                targetSpawnnerScript.inStartArea = true; // Set the flag to indicate the controllre is now in the start area
+                initTime = Time.time; // set the start time
+                                      //Debug.Log("Init Time: " + initTime);
+            //}
         }
     }
+
+
 }
