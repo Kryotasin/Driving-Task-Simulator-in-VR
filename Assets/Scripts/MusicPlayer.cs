1b776896169bc1fdb4fd0951cb10f667a62b0fe6
Kshitij Sinha <kshitij.sinha94@gmail.com>
2019-04-29 01:53:12 -0400

    Editing Music Player

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MusicPLayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioClip clips;
     void Start()
     {
         
