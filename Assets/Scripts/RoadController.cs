5675ced9bc8d0f52f61ff9a50f59b0938b2d6859
Kshitij Sinha <kshitij.sinha94@gmail.com>
2019-04-30 02:33:32 -0400

    Working Horn. Road complete.

---
+++ Assets/Scripts/RoadController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadController : MonoBehaviour
+{
+    
+    private Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
