a8910157debc4a0c544cfa5571bd60c6b723222c
Kshitij Sinha <kshitij.sinha94@gmail.com>
2019-05-02 23:06:16 -0400

    Added replacement VR Rig
    VR camera ios flipped and added new RIG since old one is not detecting controllers.

---
+++ Assets/Scripts/NotificationController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NotificationController : MonoBehaviour
+{
+
+    public GameObject notification;
+    private TextMeshPro notificationText;
+    void Start()
+    {
+        notificationText = notification.GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        notificationText.text = "Press Ok to continue";
+    }
+}
